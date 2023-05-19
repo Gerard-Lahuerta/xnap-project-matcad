4c3ddcf45dfcc120ea7948af42bfb338ea619c93
Gerard Lahuerta <gerard.lahuerta.martin@gmail.com>
2023-05-19 16:55:31 +0000

    save image?

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import wandb
 import torch
 from utils.utils import show_image
+from skimage.io import imsave
+from skimage.color import rgb2lab, lab2rgb, rgb2gray
+import numpy as np
 
 
 def test(model, test_loader, device="cuda", save:bool= True):
@@ -56,15 +59,10 @@ def test_model1(model, test, label, criterion, device="cuda", save:bool= True):
     #show_image(label)
     #show_image(output)
 
-    torch.onnx.export(model,  # model being run
-                          output,  # model input (or a tuple for multiple inputs)
-                          "model.onnx",  # where to save the model (can be a file or file-like object)
-                          export_params=True,  # store the trained parameter weights inside the model file
-                          opset_version=10,  # the ONNX version to export the model to
-                          do_constant_folding=True,  # whether to execute constant folding for optimization
-                          input_names=['input'],  # the model's input names
-                          output_names=['output'],  # the model's output names
-                          dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
-                                        'output': {0: 'batch_size'}})
+    cur = np.zeros((400, 400, 3))
+    cur[:,:,0] = test[0][0,:,:].cpu()
+    cur[:,:,1:] = output[0].cpu()
+    imsave("img_result.png", lab2rgb(cur))
+    imsave("img_gray_version.png", rgb2gray(lab2rgb(cur)))
 
     #wandb.log({"Loss": loss})
