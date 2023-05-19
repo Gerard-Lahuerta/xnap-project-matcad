a814dca8af40518b68feafd4289d132cd2889378
Gerard <gerard.lahuerta.martin@gmail.com>
2023-05-19 18:22:36 +0200

    TRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY

--- test.py
+++ test.py
@@ -53,7 +53,18 @@ def test_model1(model, test, label, criterion, device="cuda", save:bool= True):
 
     # display the epoch training loss
     print("Test loss = {:.6f}".format(loss))
-    show_image(label)
-    show_image(output)
+    #show_image(label)
+    #show_image(output)
+
+    torch.onnx.export(model,  # model being run
+                          output,  # model input (or a tuple for multiple inputs)
+                          "model.onnx",  # where to save the model (can be a file or file-like object)
+                          export_params=True,  # store the trained parameter weights inside the model file
+                          opset_version=10,  # the ONNX version to export the model to
+                          do_constant_folding=True,  # whether to execute constant folding for optimization
+                          input_names=['input'],  # the model's input names
+                          output_names=['output'],  # the model's output names
+                          dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
+                                        'output': {0: 'batch_size'}})
 
     #wandb.log({"Loss": loss})
