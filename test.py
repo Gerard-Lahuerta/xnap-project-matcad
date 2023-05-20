0ea1eb5a8846c90647d9618ad7af59b8bee4bc0d
Gerard Lahuerta <gerard.lahuerta.martin@gmail.com>
2023-05-20 16:16:51 +0000

    Alguns canvis en el train i test....

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from skimage.io import imsave
 from skimage.color import rgb2lab, lab2rgb, rgb2gray
 import numpy as np
 
-
+'''
 def test(model, test_loader, device="cuda", save:bool= True):
     # Run the model on some test examples
     with torch.no_grad():
@@ -36,12 +36,40 @@ def test(model, test_loader, device="cuda", save:bool= True):
                           dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
                                         'output': {0: 'batch_size'}})
         wandb.save("model.onnx")
-
+'''
 
 ########################################################################################################################
 
 
-def test_model1(model, test, label, criterion, device="cuda", save:bool= True):
+def test(model):
+
+    if model.get_name() == "Model 1":
+        test_model = test_model_1
+    elif model.get_name() == "Model 2":
+        test_model = test_model_2
+    elif model.get_name() == "Model 3":
+        test_model = test_model_3
+    else:
+        assert(False)
+
+    model.eval()
+
+    with torch.no_grad():
+        correct, total = 0, 0
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print(f"Accuracy of the model on the {total} " +
+              f"test images: {correct / total:%}")
+        
+        wandb.log({"test_accuracy": correct / total})
+
+
+def test_model_1(model, test, label, criterion, device="cuda", save:bool= True):
     model.eval()
 
     test = test.to(device)
@@ -56,16 +84,16 @@ def test_model1(model, test, label, criterion, device="cuda", save:bool= True):
     # display the epoch training loss
     print("Test loss = {:.6f}".format(loss))
 
-
-    cur = np.zeros((400, 400, 3))
-    cur[:,:,0] = np.array(test[0][0,:,:].cpu())
-    cur[:,:,1:] = np.array(128*output[0].cpu().permute(2,1,0))
-    imsave("results/"+model.get_name()+"/img_result.png", (lab2rgb(cur)*255).astype(np.uint8))
-    imsave("results/"+model.get_name()+"/img_gray_version.png", ((rgb2gray(lab2rgb(cur)))*255).astype(np.uint8))
+    if(save):
+        cur = np.zeros((400, 400, 3))
+        cur[:,:,0] = np.array(test[0][0,:,:].cpu())
+        cur[:,:,1:] = np.array(128*output[0].cpu().permute(2,1,0))
+        imsave("results/"+model.get_name()+"/img_result.png", (lab2rgb(cur)*255).astype(np.uint8))
+        imsave("results/"+model.get_name()+"/img_gray_version.png", ((rgb2gray(lab2rgb(cur)))*255).astype(np.uint8))
     #wandb.log({"Loss": loss})
 
 
-def test_model2(model, test, criterion, device="cuda", save: bool = True):
+def test_model_2(model, test, criterion, device="cuda", save: bool = True):
     model.eval()
 
     testa = test[0].to(device)
@@ -88,3 +116,7 @@ def test_model2(model, test, criterion, device="cuda", save: bool = True):
         cur[:, :, 1:] = output[i]
         imsave("result/img_" + str(i) + ".png", lab2rgb(cur))
         # wandb.log({"Loss": loss})
+
+
+def test_model_3():
+    pass
