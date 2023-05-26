7ed661fae5928cbfc435d101c120f6cbafa56c27
Gerard Lahuerta Martín <80009126+Gerard-Lahuerta@users.noreply.github.com>
2023-05-26 19:47:36 +0200

    UTILS SUPER CUCO :DDDDDDDD

--- utils/utils.py
+++ utils/utils.py
@@ -15,23 +15,23 @@
 
 ###### IMPORTS ########################################################################################
 
-import wandb
+# model / dataset manipulations libraries
 import torch
 import torch.nn 
 import torchvision
 import torchvision.transforms as transforms
 from models.models import *
-
-import json
-from tqdm.auto import tqdm
 import random
-
-import os
 import numpy as np
-from skimage import io, color
 
+# documentative libraries
+from tqdm.auto import tqdm
+
+# image manipulation /saving libraries
+from skimage import io, color
 from skimage.io import imsave
 from skimage.color import lab2rgb
+import os
 
 
 ##### ADJUSTING DATASET ###############################################################################
@@ -41,7 +41,7 @@ def crop_center(X,cropx,cropy):
     for img in tqdm(X, desc="Adjusting Images"):
         y = img.shape[0]
         x = img.shape[1]
-        if x > cropx and y > cropy:
+        if x > cropx and y > cropy: # if some image hase less dimension than need we discard it
             startx = x//2-(cropx//2)
             starty = y//2-(cropy//2)
             img = img[starty:starty + cropy, startx:startx + cropx, :]
@@ -49,67 +49,73 @@ def crop_center(X,cropx,cropy):
     return ret
 
 def shuffle(loader):
-    p = np.random.permutation(len(loader[0]))
+    p = np.random.permutation(len(loader[0])) # random index list to permutate randomly the loader
     train = [loader[0][i] for i in p]
     test = [loader[1][i] for i in p]
     return [train, test]
 
 def get_data_model(path, split = 0.95, train = True):
 
+    # FRAGMENTATION OF THE DATASET
     X = []
     for filename in os.listdir(path):
-        X.append(io.imread(path + filename)[:,:,0:3])
+        X.append(io.imread(path + filename)[:,:,0:3]) 
 
-    split = int(split * len(X))
-    if not train:
+    split = int(split * len(X)) # select a fragment of the hole dataset
+
+    if not train: # select the last fragment of the dataset
         split = 1-split
         X = X[split:]
-    else:
+    else: #select the first fragment of the dataset
         X = X[:split]
 
-    size = X[0].shape
-
-    if size[0] != size[1]:
+    # DETERMINING SIZE OF IMAGE
+    size = X[0].shape 
+    if size[0] != size[1]: # not simetric image --> ajust image to make it simetric (crop it)
         size = 256
         X = crop_center(X,256,256)
-    else:
+    else: 
         size = size[0]
 
+    # TRANSFORMING DATASET AND SEPARATION DATA (X) AND LABEL(Y)
     transform = transforms.ToTensor()
 
-    Xtest = color.rgb2lab(1.0 / 255 * np.array(X, dtype="float"))[:, :, :, 0]
-
-    Xtest = Xtest.reshape(Xtest.shape + (1,))
+    ## data X separation (L image)
+    L = color.rgb2lab(1.0 / 255 * np.array(X, dtype="float"))[:, :, :, 0]
+    L = L.reshape(L.shape + (1,))
 
     aux = []
-    for i in Xtest:
+    for i in L:
         aux.append(transform(i).float().reshape(1, 1, size, size))
-    Xtest = aux
+    L = aux
 
-    Ytest = color.rgb2lab(1.0 / 255 * np.array(X, dtype="float"))[:, :, :, 1:]
-    Ytest = Ytest / 128
+    ## data Y separation (AB image)
+    AB = color.rgb2lab(1.0 / 255 * np.array(X, dtype="float"))[:, :, :, 1:]
+    AB = AB / 128
+
     aux = []
-    for i in Ytest:
+    for i in AB:
         aux.append(transform(i).float().reshape(1, 2, size, size))
-    Ytest = aux
+    AB = aux
 
-    return [Xtest, Ytest]
+    return [L, AB]
 
 def get_data(config):
-    if config["data_set"] == "default":
+    if config["data_set"] == "default": # default mode --> original dataset of each model
         if config["model"] == "Model 1":
             train = get_data_model("data/data_1/", split = 0.5)
-            test = get_data_model("data/data_1/", split = 0.5)
+            test = get_data_model("data/data_1/", split = 0.5, train = False)
 
         elif config["model"] == "Model 2":
             train = get_data_model("data/data_2/Train/", split = 1)
+            # do not put the train=False parameter because test dataset is in a separate folder
             test = get_data_model("data/data_2/Test/", split = 1)
 
         else: # Model 3
             train = get_data_model("data/data_2/Train/", split = 1)
             test = get_data_model("data/data_2/Train/", split = 1)
 
-    else:
+    else: # selecction custom dataset and train split
         train = get_data_model(config["data_set"], split = config["split"])
         test = get_data_model(config["data_set"], split = 0.01, train = False)
 
@@ -121,47 +127,51 @@ def get_data(config):
 def built_model(config, device="cuda"):
     if config["model"] == "Model 1":
         model = Model1().to(device)
+
     elif config["model"] == "Model 2":
         model = Model2().to(device)
-    elif config["model"] == "Model 3": # Model 3
+
+    elif config["model"] == "Model 3":
         model = Model3().to(device) 
+
     elif config["model"] == "ConVAE":
         model = ConvAE().to(device)
-    else:
+
+    else: # ColorizationNet by default mode
         model = ColorizationNet().to(device)
 
     return model
 
 def RMSELoss(yhat,y):
-    return torch.sqrt(torch.mean((yhat-y)**2))
+    return torch.sqrt(torch.mean((yhat-y)**2)) 
 
 def set_criterion(config):
-    if config["criterion"] == "MSE":
-        criterion = torch.nn.MSELoss()
+    if config["criterion"] == "RMSE":
+        criterion = RMSELoss
 
     elif config["criterion"] == "MAE":
         criterion = torch.nn.L1Loss()
 
-    else:
-        criterion = RMSELoss
+    else: # MSE loss function by default mode
+        criterion = torch.nn.MSELoss()
     
     return criterion
 
 def set_optimizer(config, model):
-    if config["optimizer"] == "Adam":
-        optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"]) # <-- ñe
+    if config["optimizer"] == "Adagrad":
+        optimizer = torch.optim.Adagrad(model.parameters(), lr=config["learning_rate"])
     
     elif config["optimizer"] == "RMSprop":
         optimizer = torch.optim.RMSprop(model.parameters(), lr=config["learning_rate"], momentum=0.9)
     
     elif config["optimizer"] == "Adadelta":
-        optimizer = torch.optim.Adadelta(model.parameters(), lr=config["learning_rate"]) # <-- kk
+        optimizer = torch.optim.Adadelta(model.parameters(), lr=config["learning_rate"])
     
     elif config["optimizer"] == "SGD":
         optimizer = torch.optim.SGD(model.parameters(), lr=config["learning_rate"], momentum=0.9)
     
-    elif config["optimizer"] == "Adagrad":
-        optimizer = torch.optim.Adagrad(model.parameters(), lr=config["learning_rate"])
+    else: # Adam optimizer by default mode
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
     
     return optimizer
     
@@ -172,7 +182,7 @@ def make(config, device = "cuda"):
     criterion = set_criterion(config)
     optimizer = set_optimizer(config, model)
 
-    return model, train, test, criterion, optimizer#, scheduler
+    return model, train, test, criterion, optimizer
  
 
 ##### DATA SAVING #####################################################################################
@@ -183,20 +193,20 @@ def save_image(output_AB, output_L, size, path, name = "/img_"):
         save_1_image(AB, L, size, path, name+str(i+1))
 
 def save_1_image(AB, X, size, path, name):
-    cur = np.zeros((size, size, 3))
-    cur[:, :, 0] = np.array(X[0][0, :, :].cpu())
-    cur[:, :, 1:] = np.array(128 * AB[0].cpu().permute(1, 2, 0))
-    imsave(path + name + ".png", (lab2rgb(cur) * 255).astype(np.uint8))
+    cur = np.zeros((size, size, 3)) # create a size x size x 3 (the 3th dimension corresponding to LAB)
+    cur[:, :, 0] = np.array(X[0][0, :, :].cpu()) # add the L component 
+    cur[:, :, 1:] = np.array(128 * AB[0].cpu().permute(1, 2, 0)) # add the AB dimensions (model output)
+    imsave(path + name + ".png", (lab2rgb(cur) * 255).astype(np.uint8)) # saving image in its folder
 
 def save_model(model):
-    path = "weights/Weights "+model.get_name()+".pt"
+    path = "weights/Weights "+model.get_name()+".pt" # path + file name of the model weights to save
     torch.save(model.state_dict(), path)
 
 def import_model(model):
-    path = "weights/Weights "+model.get_name()+".pt"
-    model.load_state_dict(torch.load(path))
+    path = "weights/Weights "+model.get_name()+".pt" # path + file name of the model weights
+    model.load_state_dict(torch.load(path)) # importing trained model
     return model
 
 def delete_files(dir = "image_log"):
     for f in os.listdir(dir):
-        os.remove(os.path.join(dir, f))
+        os.remove(os.path.join(dir, f)) # deleting file of the "dir" folder
