6625f3f7aa589c86a536c9d821e9f5b7eac08950
Ona Sánchez Núñez <onasancheznunez@gmail.com>
2023-05-26 20:08:02 +0200

    TEST BONICOOOOOO

--- test.py
+++ test.py
@@ -11,82 +11,57 @@
 
     Information about the program in: https://github.com/DCC-UAB/xnap-project-matcad_grup_6.git
 """
+
+
+###### IMPORTS #########################################################################################################
+
 import wandb
 import torch
 
 from utils.utils import save_image
 from tqdm.auto import tqdm
 
-'''
-def test(model, test_loader, device="cuda", save:bool= True):
-    # Run the model on some test examples
-    with torch.no_grad():
-        correct, total = 0, 0
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
 
-        print(f"Accuracy of the model on the {total} " +
-              f"test images: {correct / total:%}")
-        
-        wandb.log({"test_accuracy": correct / total})
+###### TESTING OF THE MODEL ############################################################################################
 
-    if save:
-        
-        # Save the model in the exchangeable ONNX format
-        torch.onnx.export(model,  # model being run
-                          images,  # model input (or a tuple for multiple inputs)
-                          "model.onnx",  # where to save the model (can be a file or file-like object)
-                          export_params=True,  # store the trained parameter weights inside the model file
-                          opset_version=10,  # the ONNX version to export the model to
-                          do_constant_folding=True,  # whether to execute constant folding for optimization
-                          input_names=['input'],  # the model's input names
-                          output_names=['output'],  # the model's output names
-                          dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
-                                        'output': {0: 'batch_size'}})
-        wandb.save("model.onnx")
-'''
-
-########################################################################################################################
-
-
-def test_model(model, test_loader, criterion, save:bool = True):
-        
-    model.eval()
-    size = test_loader[0][0].shape[2]
-
-    with torch.no_grad():
-        output_AB, output_L, loss = test(model, test_loader, criterion)
-        wandb.log({"Test Loss": loss})
-
-    if save:
-        path = "results/"+model.get_name()
-        save_image(output_AB, output_L, size, path)
-
-     
-
-def test(model, test, criterion, device="cuda", save: bool = True):
+def test(model, test, criterion, device="cuda"):
     model.eval()
 
+    # Inicialization of parameters
     loss = 0
     output = []
     input = []
     n = len(test[0])
 
-
+    # Tracking the evolution of the test
     test = tqdm(zip(test[0], test[1]), desc="Testing "+model.get_name())
+
+    # Testing images
     for L, AB in test:
         X = L.to(device)
         Y = AB.to(device)
 
         with torch.no_grad():
             out = model(X)
+
         # compute training reconstruction loss
         loss += criterion(out, Y)
-        input.append(X)
-        output.append(out)
+        input.append(X) # list with the greyscale images
+        output.append(out) # resulting colorized images
 
     return output, input, loss/n
+
+def test_model(model, test_loader, criterion, save: bool = True):
+    # Set model in evaluation mode
+    model.eval()
+    size = test_loader[0][0].shape[2]
+
+    # Run testing and track with wandb
+    with torch.no_grad():
+        output_AB, output_L, loss = test(model, test_loader, criterion)
+        wandb.log({"Test Loss": loss})
+
+    # Saving the resulting colorized images
+    if save:
+        path = "results/" + model.get_name()
+        save_image(output_AB, output_L, size, path)
